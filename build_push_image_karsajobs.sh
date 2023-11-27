5d78569417917bffdc9dfd252206aebc1b90fa01
gunzxx <ggm5850@gmail.com>
2023-11-27 19:50:25 +0700

    update env dan github token

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -3,7 +3,7 @@ docker build -t karsajobs-ui:latest .
 # membuat alias untuk image karsajobs-ui agar dapat di upload pada github packages
 docker tag karsajobs-ui:latest ghcr.io/gunzxx/karsajobs-ui:latest
 # membuat environment variabel yaitu PAT untuk menyimpan token github packages
-export PAT=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
+export PAT=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
 # melakukan login pada github packages
 echo $PAT | docker login ghcr.io --username gunzxx --password-stdin
 # melakukan push image ke github packages
